31c5a02755a3d58937faae7674fbb2a71def9343
deonlobo <deonvictorlobo@gmail.com.com>
2019-11-10 11:14:33 +0530

    Updates

---
+++ precache-manifest.2bad380081d71d76dd270771a1fd47be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a7b2b5e6bfee4aeac00308db3f2156",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3bdcf827f52426934774",
+    "url": "/robofriends/static/css/main.2d3c57f0.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "3bdcf827f52426934774",
+    "url": "/robofriends/static/js/main.694c1ad8.chunk.js"
+  },
+  {
+    "revision": "d7d3c3ad781ab5e09c65",
+    "url": "/robofriends/static/js/runtime-main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
